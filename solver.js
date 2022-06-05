77eb5014b37f980a3f20e24151b53c1878f9ed73
isuckatcs <65320245+isuckatcs@users.noreply.github.com>
2022-06-06 00:34:05 +0200

    improved UX

--- solver.js
+++ solver.js
@@ -1,24 +1,30 @@
 {
-    let htmlManip = function () {
+    let htmlManip = () => {
+        let defaultTime = 5.4;
+
         let html =
-            '<h3 class="UIHeading--three" style="text-align: center; margin: 35px 0 15px 0; width: 90%; position: relative; left: 50%; transform: translateX(-50%);">What time do you wish to achieve?</h3><p style="text-align: center; margin: 0 0 35px 0; width: 90%; position: relative; left: 50%; transform: translateX(-50%);">Note that Quizlet can\'t store a time lower than 0.5s</p><input type="text" id="time" name="time" value="--" style="color: #455358; -webkit-appearance: none;-moz-appearance: none; appearance: none; border: solid #3ccfcf;border-radius: .25rem;display: inline-block;font: inherit;max-width: 100%;padding: .75rem 1.5rem; font-weight: 600; font-size: .875rem; line-height: 1.285714285714286; transition: all .12s cubic-bezier(.47,0,.745,.715); width: 80px; height:50px; position:relative; left:50%;transform:translateX(-50%); text-align:center; padding:0px;"></input>';
+            '<h3 class="UIHeading--three" style="text-align: center; margin: 35px 0 15px 0; width: 90%; position: relative; left: 50%; transform: translateX(-50%);">What time do you wish to achieve?</h3><p style="text-align: center; margin: 0 0 35px 0; width: 90%; position: relative; left: 50%; transform: translateX(-50%);">Note that Quizlet can\'t store a time lower than 0.5s</p><input type="text" id="time" name="time" value="%default%" style="color: #455358; -webkit-appearance: none;-moz-appearance: none; appearance: none; border: solid #3ccfcf;border-radius: .25rem;display: inline-block;font: inherit;max-width: 100%;padding: .75rem 1.5rem; font-weight: 600; font-size: .875rem; line-height: 1.285714285714286; transition: all .12s cubic-bezier(.47,0,.745,.715); width: 80px; height:50px; position:relative; left:50%;transform:translateX(-50%); text-align:center; padding:0px; margin: 0 0 0 0"></input>';
+
+        html = html.replace('%default%', defaultTime);
+
         document
             .querySelector('.UIModalBody')
             .insertAdjacentHTML('beforeend', html);
-        document.getElementById('time').addEventListener('click', function (e) {
-            e.target.value = '';
-        });
+
+        document
+            .getElementById('time')
+            .addEventListener('click', e => (e.target.value = ''));
     };
 
-    let getTerms = function () {
+    let getTerms = () => {
         let def = [],
             word = [],
             terms = Quizlet.matchModeData.terms;
 
-        for (let i = 0; i < terms.length; i++) {
-            def[i] = terms[i].word;
-            word[i] = terms[i].definition;
-        }
+        terms.forEach((el, index) => {
+            def[index] = el.definition;
+            word[index] = el.word;
+        });
 
         return {
             def: def,
@@ -26,13 +32,11 @@
         };
     };
 
-    let getObjects = function () {
-        let text = [],
+    let getObjects = () => {
+        let text,
             elem = document.querySelectorAll('.MatchModeQuestionGridTile');
 
-        for (let i = 0; i < 12; i++) {
-            text[i] = elem[i].innerText;
-        }
+        text = Array.prototype.map.call(elem, x => x.innerText);
 
         return {
             text,
@@ -40,16 +44,16 @@
         };
     };
 
-    let findPair = function (current, terms) {
+    let findPair = (current, terms) => {
         for (let i = 0; i < terms.def.length; i++) {
             if (current === terms.def[i]) return terms.word[i];
             else if (current === terms.word[i]) return terms.def[i];
         }
     };
 
-    let bot = function (botTerms, botObjects) {
-        let event = new PointerEvent('pointerdown'),
-            clicked = [],
+    let bot = (botTerms, botObjects) => {
+        let event = new PointerEvent('pointerdown');
+        let clicked = [],
             x = 0;
 
         while (x < botObjects.text.length) {
@@ -59,41 +63,53 @@
                 let index = botObjects.text.indexOf(
                     findPair(botObjects.text[x], botTerms)
                 );
-                clicked.push(index);
 
                 botObjects.elem[index].dispatchEvent(event);
+
+                clicked.push(index);
             }
+
             x++;
         }
     };
 
-    let callBack = function () {
-        let time = document.getElementById('time').value;
+    let callBack = time => {
+        let delay = parseInt(time * 1000 + 40);
 
-        if (time !== '--' && time !== '') {
-            let delay = parseInt(time * 1000);
-            delay += 40;
-
-            setTimeout(function () {
-                bot(getTerms(), getObjects());
-            }, delay);
-        }
+        setTimeout(() => bot(getTerms(), getObjects()), delay);
     };
 
-    let init = function () {
+    let init = () => {
         htmlManip();
 
-        document
-            .querySelector('.UIButton--hero')
-            .addEventListener('click', callBack);
+        let defaultTime = document.getElementById('time').value;
 
-        document
+        let time = document
             .getElementById('time')
-            .addEventListener('keydown', function (e) {
-                if (e.keyCode === 13) {
-                    document.querySelector('.UIButton--hero').click();
-                }
-            });
+            .addEventListener(
+                'keyup',
+                () => (time = document.getElementById('time').value)
+            );
+
+        document.getElementById('time').addEventListener('keydown', e => {
+            if (e.keyCode === 13) {
+                document.querySelector('.UIButton--hero').click();
+            }
+        });
+
+        const target = document.querySelector('body');
+
+        let config = {
+            attributes: true,
+        };
+
+        let observer = new MutationObserver(() => {
+            if (time >= 0) {
+                defaultTime = time;
+            }
+
+            callBack(defaultTime);
+        }).observe(target, config);
     };
 
     init();
